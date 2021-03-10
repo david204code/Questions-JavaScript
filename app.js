5af7400534d2682d0ff3c9c01e43d9a2546b761f
David Wu <david204code@gmail.com>
2021-03-10 17:25:38 +0000

    traversing the dom, select the element's parentElement and using toggle to add/remove show-text

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
+// traversing the dom means moving up and down the dom tree
+const btns = document.querySelectorAll(".question-btn");
+// console.log(btns);
+// loop over them
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    // console.log(e.currentTarget.parentElement.parentElement);
+    const question = e.currentTarget.parentElement.parentElement;
+    // toggle add the show-text css to the question element...toggle mean add or remove
+    question.classList.toggle("show-text");
+  });
+});
+
 //using selectors inside the element
-// traversing the dom
