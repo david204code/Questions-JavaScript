6122dce52b1cd592eac91ad6e6bb46011ce7c22f
David Wu <david204code@gmail.com>
2021-03-10 17:34:07 +0000

    using selectors inside the element example

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 // traversing the dom means moving up and down the dom tree
-const btns = document.querySelectorAll(".question-btn");
-// console.log(btns);
-// loop over them
-btns.forEach(function (btn) {
-  btn.addEventListener("click", function (e) {
-    // console.log(e.currentTarget.parentElement.parentElement);
-    const question = e.currentTarget.parentElement.parentElement;
-    // toggle add the show-text css to the question element...toggle mean add or remove
+// const btns = document.querySelectorAll(".question-btn");
+// // console.log(btns);
+// // loop over them
+// btns.forEach(function (btn) {
+//   btn.addEventListener("click", function (e) {
+//     // console.log(e.currentTarget.parentElement.parentElement);
+//     const question = e.currentTarget.parentElement.parentElement;
+//     // toggle add the show-text css to the question element...toggle mean add or remove
+//     question.classList.toggle("show-text");
+//   });
+// });
+
+//using selectors inside the element
+const questions = document.querySelectorAll(".question");
+// console.log(questions);
+
+// loop over the nodelist of questions
+questions.forEach(function (question) {
+  // console.log(question);
+  const btn = question.querySelector(".question-btn");
+  // console.log(btn);
+  btn.addEventListener("click", function () {
     question.classList.toggle("show-text");
   });
 });
-
-//using selectors inside the element
